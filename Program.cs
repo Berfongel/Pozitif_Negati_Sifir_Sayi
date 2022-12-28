59e338df3f5243f1d07dfef027c7d89525e8a930
Berfongel <121492984+Berfongel@users.noreply.github.com>
2022-12-28 15:12:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+int sayi;
+Console.WriteLine("Lütfen bir sayı giriniz");
+sayi = Convert.ToInt32(Console.ReadLine());
+//girilen sayı 0 dan büyükse pozitif değilse negatif.
+if (sayi>0)
+{
+    Console.WriteLine("Girdiğiniz sayı pozitif bir sayıdır.");
+}
+else if(sayi==0)
+{
+    Console.WriteLine("Girdiğiniz sayı ne negatif ne pozitiftir.");
+}
+else 
+{
+    Console.WriteLine("Girdiğiniz sayı negatif bir sayıdır.");
+}
+    
